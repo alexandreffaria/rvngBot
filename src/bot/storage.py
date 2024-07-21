6e08377e62f352d9a97cc6bbfbc84471905615e9
Alexandre Faria <alexandreffaria@me.com>
2024-07-20 22:07:49 -0300

    in memory storage

---
+++ src/bot/storage.py
@@ -0,0 +1,11 @@
+class Storage:
+    def __init__(self):
+        self.data = {}
+
+    def set(self, user_id, key, value):
+        if user_id not in self.data:
+            self.data[user_id] = {}
+        self.data[user_id][key] = value
+
+    def get(self, user_id, key):
+        return self.data.get(user_id, {}).get(key, None)
